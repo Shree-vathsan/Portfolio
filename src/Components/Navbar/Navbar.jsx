440e2827ca016660c598c9da6196482464ee2e76
Shree-vathsan <shreevathsan1904@gmail.com>
2024-11-27 19:20:30 +0530

    Initial commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,47 +1,51 @@
 import React from 'react';
 import './Navbar.css';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import menu_icon from '../../Assets/menu.png'
-import menu_close from '../../Assets/menu_close.png'
+import menu_icon from '../../Assets/menu.png';
+import menu_close from '../../Assets/menu_close.png';
 
 const Navbar = () => {
   const [menu, setMenu] = React.useState('home');
 
+  const handleScroll = (id) => {
+    setMenu(id);
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
+
   return (
     <div className="navbar">
-      <h1 className='logo'>SV</h1>
-      <img src={menu_icon} alt="" height={20} width={20} className='nav-mob-open'/>
+      <h1 className="logo">SV</h1>
+      <img src={menu_icon} alt="" height={20} width={20} className="nav-mob-open" />
       <ul className="nav-menu">
-        <img src={menu_close} alt="" height={30} width={30} className='nav-mob-close' />
+        <img src={menu_close} alt="" height={30} width={30} className="nav-mob-close" />
         <li>
-          <AnchorLink className="anchor-link" href="#home">
-            <p onClick={() => setMenu("home")}>Home</p>
-          </AnchorLink>
+          <p className="anchor-link" onClick={() => handleScroll('home')}>
+            Home
+          </p>
         </li>
         <li>
-          <AnchorLink className="anchor-link" offset={50} href="#about">
-            <p onClick={() => setMenu("about")}>About</p>
-          </AnchorLink>
+          <p className="anchor-link" onClick={() => handleScroll('about')}>
+            About
+          </p>
         </li>
         <li>
-          <AnchorLink className="anchor-link" offset={50} href="#services">
-            <p onClick={() => setMenu("services")}>Services</p>
-          </AnchorLink>
+          <p className="anchor-link" onClick={() => handleScroll('services')}>
+            Services
+          </p>
         </li>
         <li>
-          <AnchorLink className="anchor-link" offset={50} href="#work">
-            <p onClick={() => setMenu("work")}>Work</p>
-          </AnchorLink>
+          <p className="anchor-link" onClick={() => handleScroll('work')}>
+            Work
+          </p>
         </li>
         <li>
-          <AnchorLink className="anchor-link" offset={50} href="#contact">
-            <p onClick={() => setMenu("contact")}>Contact</p>
-          </AnchorLink>
+          <p className="anchor-link" onClick={() => handleScroll('contact')}>
+            Contact
+          </p>
         </li>
       </ul>
-      {/* <div className="nav-connect">
-        Connect With Me
-      </div> */}
     </div>
   );
 };
