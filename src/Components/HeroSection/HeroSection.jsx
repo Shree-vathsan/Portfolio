aeb03f79607e0e59252154dbe91fef5c9292d4e4
Shree-vathsan <shreevathsan1904@gmail.com>
2024-11-28 22:08:29 +0530

    Your commit message

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import './HeroSection.css';
 import Vathsan from '../../Assets/Vathsan.jpg';
+
 const HeroSection = () => {
   return (
     <div id='home' className='hero'>
@@ -8,11 +9,13 @@ const HeroSection = () => {
         <h1>Hi!, I'm <span>Shreevathsan</span></h1>
         <p>Driven Computer Science Engineering student eager to create impactful digital solutions, combining creativity, technical skills, and a love for coding.</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect with me</div>
+            <a href="https://www.linkedin.com/in/shreevathsan-r-91183625b/" target="_blank" rel="noopener noreferrer" className="hero-connect">
+                Connect with me
+            </a>
             <div className="hero-resume">My Resume</div>
         </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
