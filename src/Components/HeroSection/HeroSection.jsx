e13a71c416a87f87d6599ad45a4782d44fc3c461
Shree-vathsan <shreevathsan1904@gmail.com>
2024-11-28 14:54:54 +0530

    Your commit message here

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <div id='home' className='hero'>
         <img src={Vathsan} alt="" height={350} width={275}/>
-        <h1><span>Hi!, I'm Shreevathsan</span></h1>
+        <h1>Hi!, I'm <span>Shreevathsan</span></h1>
         <p>Driven Computer Science Engineering student eager to create impactful digital solutions, combining creativity, technical skills, and a love for coding.</p>
         <div className="hero-action">
             <div className="hero-connect">Connect with me</div>
